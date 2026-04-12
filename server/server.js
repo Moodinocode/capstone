22db11e00c59ba6d6292176ee6d92ebb29e2d383
Moodinocode <mehdi21092005@gmail.com>
2026-04-12 20:30:59 +0300

    fix: trust proxy for Render deployment (rate limit X-Forwarded-For)

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ import scheduleRoutes from './routes/scheduleRoutes.js';
 
 const app = express();
 
+app.set('trust proxy', 1);
 app.use(helmet());
 const allowedOrigins = process.env.FRONTEND_URL
   ? [process.env.FRONTEND_URL]
